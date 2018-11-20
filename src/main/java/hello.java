63f657bcdaa59be3b15cdb6b0a56d215958361fb
whailong.wang <whailong.wang@qunar.com>
2018-11-20 19:01:41 +0800

    2

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.print("000000");
         System.out.print("22222");
+        System.out.print("000000");
+        System.out.print("22222");
     }
 }
